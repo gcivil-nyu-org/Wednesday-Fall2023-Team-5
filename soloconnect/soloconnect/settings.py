75a0b7f6305c30b2c58f5ed6748e365ced3fa2f0
Mike Zaharchenko <mz2433@nyu.edu>
2023-10-08 21:19:16 -0400

    Housekeeping: added a templates directory and set up a very basic base template as well as veeeerrrry basic homepage

--- soloconnect/soloconnect/settings.py
+++ soloconnect/soloconnect/settings.py
@@ -15,7 +15,6 @@ import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
@@ -55,7 +54,7 @@ ROOT_URLCONF = 'soloconnect.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
--- soloconnect/user_profile/views.py
+++ soloconnect/user_profile/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from . import models
 
 # Create your views here.
+
+# User Profile CRUD
+def create_user(request):
+    pass
