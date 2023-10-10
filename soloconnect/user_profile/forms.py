04a70acb7eb754a47faad2a962dc924092a6a80f
Mike Zaharchenko <mz2433@nyu.edu>
2023-10-10 18:29:42 -0400

    Non-database files transferred from feature-profile-creation

---
+++ soloconnect/user_profile/forms.py
@@ -0,0 +1,16 @@
+from django.contrib.auth.forms import UserCreationForm
+from django import forms
+from .models import CustomUser, UserProfile
+
+
+class AccountRegistrationForm(UserCreationForm):
+    email = forms.EmailField(required=True)
+    class Meta:
+        model = CustomUser
+        fields = ("first_name", "last_name", "email")
+
+class ProfileUpdateForm(forms.ModelForm):
+    class Meta:
+        model = UserProfile
+        fields = ("dob", "bio", "university")
+
