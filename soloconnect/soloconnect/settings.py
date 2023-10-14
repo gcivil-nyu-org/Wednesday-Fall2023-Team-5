f57350a0c0e8826b08490a17e9d08fb52e12f316
kravisankaran <kumudriyer@gmail.com>
2023-10-13 23:21:10 -0400

    fixing formatting

--- soloconnect/soloconnect/settings.py
+++ soloconnect/soloconnect/settings.py
@@ -30,7 +30,7 @@ ALLOWED_HOSTS = [
     "127.0.0.1",
     "soloconnect-integration.us-west-2.elasticbeanstalk.com",
     "soloconnect-production.us-west-2.elasticbeanstalk.com",
-    "soloconnect-dev.us-west-2.elasticbeanstalk.com"
+    "soloconnect-dev.us-west-2.elasticbeanstalk.com",
 ]
 
 # Application definition
