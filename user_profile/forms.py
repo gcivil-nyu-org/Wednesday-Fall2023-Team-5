67adea1e1722eb641a459ba27bcc293bba16c1b3
Mike Zaharchenko <mz2433@nyu.edu>
2023-10-18 16:17:56 -0400

    Added a rudimentary .edu email verification feature

--- user_profile/forms.py
+++ user_profile/forms.py
@@ -1,11 +1,21 @@
 from django import forms
 from django.contrib.auth.forms import UserCreationForm
-
+from django.core.exceptions import ValidationError
+from .helpers import email_is_valid
 from .models import User, UserProfile
 
 
 class AccountRegistrationForm(UserCreationForm):
     email = forms.EmailField(required=True)
+
+    def clean(self):
+        cleaned_data = self.cleaned_data
+        email = cleaned_data.get("email")
+
+        if not email_is_valid(email):
+            raise ValidationError("Email must end in .edu")
+
+        return cleaned_data
 
     class Meta:
         model = User
