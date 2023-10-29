b7944d20d72a7242eed9d4dbdae47a8d8ca09448
kravisankaran <kumudriyer@gmail.com>
2023-10-29 15:38:23 -0400

    black lint fix

--- createsu.py
+++ createsu.py
@@ -4,9 +4,5 @@ from django.core.management.base import BaseCommand
 
 class Command(BaseCommand):
     def handle(self, *args, **options):
-        if not User.objects.filter(
-            username="scadmin@nyu.edu"
-        ).exists():
-            User.objects.create_superuser(
-                "scadmin@nyu.edu", "adminSC7$"
-            )
+        if not User.objects.filter(username="scadmin@nyu.edu").exists():
+            User.objects.create_superuser("scadmin@nyu.edu", "adminSC7$")
