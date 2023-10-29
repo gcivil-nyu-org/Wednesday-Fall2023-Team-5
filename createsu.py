e7687d29dde3b3385f9e300b4034347306c4d867
kravisankaran <kumudriyer@gmail.com>
2023-10-29 15:23:39 -0400

    Creating superuser

---
+++ createsu.py
@@ -0,0 +1,12 @@
+from django.contrib.auth.models import User
+from django.core.management.base import BaseCommand
+
+
+class Command(BaseCommand):
+    def handle(self, *args, **options):
+        if not User.objects.filter(
+            username="scadmin@nyu.edu"
+        ).exists():
+            User.objects.create_superuser(
+                "scadmin@nyu.edu", "adminSC7$"
+            )
