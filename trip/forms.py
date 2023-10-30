60d5445c41dcd3edfdf82c36bb12f2ba528e93fc
Mike Zaharchenko <mz2433@nyu.edu>
2023-10-30 18:06:34 -0400

    Crispy fix

--- trip/forms.py
+++ trip/forms.py
@@ -3,8 +3,8 @@ from .models import UserTrip
 
 
 class UserTripCreationForm(forms.ModelForm):
-    destination_city_ef = forms.CharField()
-    destination_country_ef = forms.CharField()
+    destination_city_ef = forms.CharField(label="Destination City")
+    destination_country_ef = forms.CharField(label="Destination Country")
 
     class Meta:
         model = UserTrip
