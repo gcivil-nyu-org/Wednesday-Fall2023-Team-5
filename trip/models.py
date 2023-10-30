6484ad994eafbc6b51e41cb51880af156d89f90f
kravisankaran <kumudriyer@gmail.com>
2023-10-29 20:09:10 -0400

    Adding foreign keys

--- trip/models.py
+++ trip/models.py
@@ -1,3 +1,4 @@
+from django.contrib.auth.models import User
 from django.db import models
 
 from common import ChoiceArrayField
@@ -8,8 +9,13 @@ from constants import TRAVEL_TYPE
 class Trip(models.Model):
     destination_city = models.CharField(max_length=500)
     destination_country = models.CharField(max_length=500)
-    start_trip = models.DateTimeField("start_trip")
-    end_trip = models.DateTimeField("end_trip")
     travel_type = ChoiceArrayField(
         models.CharField(max_length=30, choices=TRAVEL_TYPE), default=list
     )
+
+
+class UserTrip(models.Model):
+    start_trip = models.DateTimeField("start_trip")
+    end_trip = models.DateTimeField("end_trip")
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    trip = models.ForeignKey(Trip, on_delete=models.CASCADE)
