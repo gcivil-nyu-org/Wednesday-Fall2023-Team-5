4c34d3e10a6ce412c9fff261f1e390d5d0f0c0b9
Mike Zaharchenko <mz2433@nyu.edu>
2023-10-30 14:49:39 -0400

    Created trip view- not yet tested

--- trip/views.py
+++ trip/views.py
@@ -1,6 +1,31 @@
-from django.shortcuts import render  # noqa
+from django.shortcuts import render, redirect  # noqa
+from django.contrib.auth.decorators import login_required
+from django.contrib import messages
+from django.urls import reverse
+
+from . import forms
+from .models import Trip
 
 
 # Create your views here.
+@login_required
 def create_trip(request):
-    pass
+    if request.method == "POST":
+        ut_creation_form = forms.UserTripCreationForm(request.POST)
+        if ut_creation_form.is_valid():
+            ut_data = ut_creation_form.cleaned_data
+            ut_instance = ut_creation_form.save(commit=False)
+
+            trip_instance, _ = Trip.objects.get_or_create(
+                destination_city=ut_data["destination_city_ef"],
+                destination_country=ut_data["destination_country_ef"],
+            )
+
+            ut_instance.trip = trip_instance
+            ut_instance.user = request.user
+            ut_instance.save()
+            messages.success(request, "Successfully created your trip")
+            return redirect(reverse("trip:view_trips"))
+
+    ut_creation_form = forms.UserTripCreationForm()
+    return render(request, "trip/create_trip.html", {"form": ut_creation_form})
