a7feb078a45b60b4cecbb5d8ec08eb24401e8b84
kravisankaran <kumudriyer@gmail.com>
2023-10-29 20:26:30 -0400

    registering model

--- trip/admin.py
+++ trip/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin  # noqa
 
-from trip.models import Trip
+from trip.models import Trip, UserTrip
 
 # Register your models here.
 
 admin.site.register(Trip)
+admin.site.register(UserTrip)
