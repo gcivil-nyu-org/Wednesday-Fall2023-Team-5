d54c1e604c560dad6cef89b0123a7b1e103b0577
Mike Zaharchenko <mz2433@nyu.edu>
2023-10-31 14:18:41 -0400

    Refactoring per suggestions. ut in variable names is now usertrip

--- trip/views.py
+++ trip/views.py
@@ -11,25 +11,25 @@ from .models import Trip
 @login_required
 def create_trip(request):
     if request.method == "POST":
-        ut_creation_form = forms.UserTripCreationForm(request.POST)
-        if ut_creation_form.is_valid():
-            ut_data = ut_creation_form.cleaned_data
-            ut_instance = ut_creation_form.save(commit=False)
+        usertrip_creation_form = forms.UserTripCreationForm(request.POST)
+        if usertrip_creation_form.is_valid():
+            usertrip_data = usertrip_creation_form.cleaned_data
+            usertrip_instance = usertrip_creation_form.save(commit=False)
 
             trip_instance, _ = Trip.objects.get_or_create(
-                destination_city=ut_data["destination_city_ef"],
-                destination_country=ut_data["destination_country_ef"],
+                destination_city=usertrip_data["destination_city_ef"],
+                destination_country=usertrip_data["destination_country_ef"],
             )
 
-            ut_instance.trip = trip_instance
-            ut_instance.user = request.user
-            ut_instance.save()
+            usertrip_instance.trip = trip_instance
+            usertrip_instance.user = request.user
+            usertrip_instance.save()
             messages.success(request, "Successfully created your trip")
             return redirect(reverse("trip:view_trips"))
     else:
-        ut_creation_form = forms.UserTripCreationForm()
+        usertrip_creation_form = forms.UserTripCreationForm()
 
-    return render(request, "trip/create_trip.html", {"form": ut_creation_form})
+    return render(request, "trip/create_trip.html", {"form": usertrip_creation_form})
 
 
 @login_required
