eaccd313c8df4190eab2f16bdfad37d62b400178
sawyer1997 <rishabhverma220898@gmail.com>
2023-11-07 19:53:23 -0500

    Fixed black

--- matching/models.py
+++ matching/models.py
@@ -5,21 +5,21 @@ from user_profile.models import User
 
 
 class UserTripMatches(models.Model):
-    sender = models.ForeignKey(User, on_delete=models.CASCADE, related_name='sent_matches')
-    receiver = models.ForeignKey(User, on_delete=models.CASCADE, related_name='receive_matches')
+    sender = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name="sent_matches"
+    )
+    receiver = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name="receive_matches"
+    )
     trip = models.ForeignKey(Trip, on_delete=models.CASCADE)
 
     MatchStatus = [
-        ('Pending', 'Pending'),
-        ('Cancelled', 'Cancelled'),
-        ('Matched', 'Matched'),
-        ('Unmatched', 'Unmatched')
+        ("Pending", "Pending"),
+        ("Cancelled", "Cancelled"),
+        ("Matched", "Matched"),
+        ("Unmatched", "Unmatched"),
     ]
 
     match_status = models.CharField(
-        max_length=10,
-        choices=MatchStatus,
-        default='Pending'
+        max_length=10, choices=MatchStatus, default="Pending"
     )
-
-
--- matching/views.py
+++ matching/views.py
@@ -2,10 +2,31 @@ from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.models import User
 from user_profile.models import UserProfile
+from .models import UserTripMatches
 
 
 @login_required
 def show_potential_matches(request, trip_id):
     all_user_profiles = UserProfile.objects.all()
-    context = {'all_user_profiles': all_user_profiles}
-    return render(request, 'matching/list_potential_matches.html', context)
+    context = {"all_user_profiles": all_user_profiles}
+    return render(request, "matching/list_potential_matches.html", context)
+
+
+@login_required
+def send_matching_request(request, trip_id, receiver_id):
+    pass
+
+
+@login_required
+def cancel_matching_request(request, trip_id, receiver_id):
+    pass
+
+
+@login_required
+def unmatch_user(request, trip_id, receiver_id):
+    pass
+
+
+@login_required
+def approve_matching_request(request, trip_id, sender_id):
+    pass
