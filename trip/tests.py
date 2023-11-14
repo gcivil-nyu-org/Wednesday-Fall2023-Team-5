0d738175270b96e421f477e692bad940bac336d0
kravisankaran <kumudriyer@gmail.com>
2023-11-14 10:52:28 -0500

    coverage

--- trip/tests.py
+++ trip/tests.py
@@ -1,3 +1,29 @@
 from django.test import TestCase  # noqa
 
 # Create your tests here.
+from datetime import datetime, timedelta
+from unittest import TestCase
+
+from trip.helpers import start_date_in_future
+
+
+class TestTrip(TestCase):
+    def test_start_date_not_in_future(self):
+        date = datetime.today().date()
+        x = start_date_in_future(date)
+        self.assertFalse(x)
+
+    def test_start_date_in_future(self):
+        date_time = datetime.today() + timedelta(24)
+        date = date_time.date()
+        x = start_date_in_future(date)
+        self.assertTrue(x)
+
+    def test_start_and_end_date_in_range(self):
+        date_time = datetime.today() + timedelta(24)
+        date = date_time.date()
+        x = start_date_in_future(date)
+        self.assertTrue(x)
+    def test_exception_not_date(self):
+        date_time = 'xxxxxx'
+        self.assertRaises(TypeError, lambda: start_date_in_future(date_time))
