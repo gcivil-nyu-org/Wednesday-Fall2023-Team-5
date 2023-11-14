a4bd78081903329ddced74efe095f1dbefd7cc5b
kravisankaran <kumudriyer@gmail.com>
2023-11-14 17:09:57 -0500

    deleting migrations

--- soloconnect/settings.py
+++ soloconnect/settings.py
@@ -112,7 +112,7 @@ if "test" in sys.argv:
     DATABASES = {
         "default": {
             "ENGINE": "django.db.backends.sqlite3",
-            "NAME": os.path.join(BASE_DIR, 'db.sqlite3'),
+             "NAME": "testmemory",
         }
     }
 # Password validation
