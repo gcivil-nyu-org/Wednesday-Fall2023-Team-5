920e6edb7f3e4ec633f8dceb30a890eb1fa5c2d9
Mike Zaharchenko <mz2433@nyu.edu>
2023-11-14 18:28:53 -0500

    Defined a new helper function to verify whether a user's age is between 18 and 99. Modified the clean method in ProfileUpdateForm to use this function and raise a validation error if age < 18 or age < 99. Modified corresponding view function (edit_profile) to display ValidationError messages if any arise.

--- user_profile/helpers.py
+++ user_profile/helpers.py
@@ -1,18 +1,4 @@
-"""
-Preference map key:
-0: hiking
-1: clubbing
-2: beach
-3: sightseeing
-4: bar hopping
-5: food tourism
-6: museums
-7: historical/cultural sites
-8: spa
-9: relaxed
-10: camping
-11: sports
-"""
+import datetime
 
 PREFERENCE_MAP = [
     "hiking",
@@ -34,3 +20,18 @@ def email_is_valid(email):
     if email.endswith(".edu") and email[0].isalpha():
         return True
     return False
+
+
+def dob_gt18_and_lt100(dob):
+    time_delta = datetime.date.today() - dob
+    age = int(time_delta.days / 365)
+    if 18 <= age < 100:
+        return True, None
+    else:
+        if age < 18:
+            return False, "You must be over 18 to use SoloConnect"
+        else:
+            return (
+                False,
+                "The age you entered is over 99. Please verify your inputs and try again",
+            )
