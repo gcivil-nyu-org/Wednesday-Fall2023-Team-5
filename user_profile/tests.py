735cd5fd5999a0f86efc425c787417d3982d032a
Mike Zaharchenko <mz2433@nyu.edu>
2023-11-15 15:52:01 -0500

    Added more unit tests

--- user_profile/tests.py
+++ user_profile/tests.py
@@ -1,5 +1,6 @@
 # Create your tests here.
 import time
+import datetime # noqa
 from django.test import TestCase
 
 from django.contrib.auth.models import User
@@ -67,6 +68,7 @@ class TestLoggedInViews(TestCase):
         User.objects.create_user(**self.credentials)
         self.client.post("/login/", self.credentials, follow=True)
 
+    # Milestone profile coverage
     def test_milestone_profile_GET(self):
         response = self.client.get(reverse("user_profile:milestone_profile"))
         self.assertEqual(response.status_code, 200)
@@ -80,3 +82,43 @@ class TestLoggedInViews(TestCase):
         )
         self.assertEqual(response.status_code, 200)
         self.assertTemplateUsed(response, "home_default/home.html")
+
+    # Edit profile coverage
+    def test_edit_profile_GET(self):
+        response = self.client.get(reverse("user_profile:edit_profile"))
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, "user_profile/edit_profile.html")
+
+    # View profile coverage
+    def test_view_profile_GET(self):
+        response = self.client.get(reverse("user_profile:view_profile"))
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, "user_profile/view_profile.html")
+
+    # Detail profile coverage
+    def test_detail_profile_GET_user_dne(self):
+        # if the user does not exist in the database, we should be redirecting to home
+        response = self.client.get(
+            reverse("user_profile:detail_profile", kwargs={"id": 30000})
+        )
+        self.assertEqual(response.status_code, 302)
+
+    def test_detail_profile_GET_user_exists(self):
+        active_user = User.objects.get(username=self.credentials["username"])
+        active_id = active_user.pk
+        response = self.client.get(
+            reverse("user_profile:detail_profile", kwargs={"id": active_id})
+        )
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, "user_profile/detail_profile.html")
+
+    def test_detail_profile_GET_user_inactive(self):
+        active_user = User.objects.get(username=self.credentials["username"])
+        active_id = active_user.pk
+        active_user.is_active = False
+        active_user.save()
+        response = self.client.get(
+            reverse("user_profile:detail_profile", kwargs={"id": active_id})
+        )
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, "user_profile/detail_profile_inactive.html")
