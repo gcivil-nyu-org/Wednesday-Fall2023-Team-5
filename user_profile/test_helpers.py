b7b2a91300443622e545638958ef944ec8742a28
Mike Zaharchenko <mz2433@nyu.edu>
2023-11-15 16:05:14 -0500

    Added updated tests to test_helpers

--- user_profile/test_helpers.py
+++ user_profile/test_helpers.py
@@ -31,3 +31,43 @@ class TestLoggedInViews(TestCase):
         )
         self.assertEqual(response.status_code, 200)
         self.assertTemplateUsed(response, "home_default/home.html")
+
+    # Edit profile coverage
+    def test_edit_profile_GET(self):
+        response = self.client.get(reverse("user_profile:edit_profile"))
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, "user_profile/edit_profile.html")
+
+    # View profile coverage
+    def test_view_profile_GET(self):
+        response = self.client.get(reverse("user_profile:view_profile"))
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, "user_profile/view_profile.html")
+
+    # Detail profile coverage
+    def test_detail_profile_GET_user_dne(self):
+        # if the user does not exist in the database, we should be redirecting to home
+        response = self.client.get(
+            reverse("user_profile:detail_profile", kwargs={"id": 30000})
+        )
+        self.assertEqual(response.status_code, 302)
+
+    def test_detail_profile_GET_user_exists(self):
+        active_user = User.objects.get(username=self.credentials["username"])
+        active_id = active_user.pk
+        response = self.client.get(
+            reverse("user_profile:detail_profile", kwargs={"id": active_id})
+        )
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, "user_profile/detail_profile.html")
+
+    def test_detail_profile_GET_user_inactive(self):
+        active_user = User.objects.get(username=self.credentials["username"])
+        active_id = active_user.pk
+        active_user.is_active = False
+        active_user.save()
+        response = self.client.get(
+            reverse("user_profile:detail_profile", kwargs={"id": active_id})
+        )
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, "user_profile/detail_profile_inactive.html")
