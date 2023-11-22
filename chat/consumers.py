9baf0ea3eacfc6cf3edffe602d900a5d32e23fd9
Mike Zaharchenko <mz2433@nyu.edu>
2023-11-22 12:49:02 -0500

    Added username handling and login protections

--- chat/consumers.py
+++ chat/consumers.py
@@ -22,25 +22,35 @@ class ChatConsumer(WebsocketConsumer):
 
         self.accept()
 
-    def disconnect(self, close_code):
-        # Leave room group
-        async_to_sync(self.channel_layer.group_discard)(
-            self.room_group_name, self.channel_name
-        )
-
-    # Receive message from WebSocket
-
     def receive(self, text_data):
         text_data_json = json.loads(text_data)
         message = text_data_json["message"]
+        username = self.scope["user"].username
 
         # Send message to room group
         async_to_sync(self.channel_layer.group_send)(
-            self.room_group_name, {"type": "chat.message", "message": message}
+            self.room_group_name,
+            {
+                "type": "chat.message",
+                "message": message,
+                "username": username
+            }
         )
 
     # Receive message from room group
     def chat_message(self, event):
         message = event["message"]
+        username = event["username"]
         # Send message to WebSocket
-        self.send(text_data=json.dumps({"message": message}))
+        self.send(text_data=json.dumps({
+            "type": 'chat',
+            "message": message,
+            "username": username,
+        }))
+
+
+    def disconnect(self, close_code):
+        # Leave room group
+        async_to_sync(self.channel_layer.group_discard)(
+            self.room_group_name, self.channel_name
+        )
