422b599d8d01e4d2afd38fc5310ab23a229e2c3d
Mike Zaharchenko <mz2433@nyu.edu>
2023-11-25 20:01:10 -0500

    Committing some progress- this doesn't work yet

--- chat/consumers.py
+++ chat/consumers.py
@@ -2,55 +2,145 @@ import json
 
 from asgiref.sync import async_to_sync
 from channels.generic.websocket import WebsocketConsumer
+from django.contrib.auth.models import User
+from common import db_retrieve_or_none
+from .models import Thread, ChatMessage
 
+# class ChatConsumer(WebsocketConsumer):
+#     def __init__(self, *args, **kwargs):
+#         super().__init__(args, kwargs)
+#         self.room_group_name = None
+#         self.room_name = None
+#         self.user = None
+#
+#     def connect(self):
+#         self.user = self.scope["user"]
+#         self.room_name = self.scope["url_route"]["kwargs"]["room_name"]
+#         self.room_group_name = f"chat_room_{self.user.username}"
+#
+#         # Join room group
+#         async_to_sync(self.channel_layer.group_add)(
+#             self.room_group_name, self.channel_name
+#         )
+#
+#         self.accept()
+#
+#     def receive(self, text_data):
+#         text_data_json = json.loads(text_data)
+#         message = text_data_json["message"]
+#         username = self.user.username
+#
+#         # Send message to room group
+#         async_to_sync(self.channel_layer.group_send)(
+#             self.room_group_name,
+#             {"type": "chat.message", "message": message, "username": username},
+#         )
+#
+#     # Receive message from room group
+#     def chat_message(self, event):
+#         message = event["message"]
+#         username = event["username"]
+#         # Send message to WebSocket
+#         self.send(
+#             text_data=json.dumps(
+#                 {
+#                     "type": "chat",
+#                     "message": message,
+#                     "username": username,
+#                 }
+#             )
+#         )
+#
+#     def disconnect(self, close_code):
+#         # Leave room group
+#         async_to_sync(self.channel_layer.group_discard)(
+#             self.room_group_name, self.channel_name
+#         )
 
 class ChatConsumer(WebsocketConsumer):
     def __init__(self, *args, **kwargs):
         super().__init__(args, kwargs)
-        self.room_group_name = None
-        self.room_name = None
         self.user = None
+        self.chat_room = None
 
     def connect(self):
-        self.room_name = self.scope["url_route"]["kwargs"]["room_name"]
-        self.room_group_name = f"chat_{self.room_name}"
         self.user = self.scope["user"]
-
-        # Join room group
+        self.chat_room = f'user_chatroom_{self.user.id}'
         async_to_sync(self.channel_layer.group_add)(
-            self.room_group_name, self.channel_name
+            self.chat_room, self.channel_layer
         )
-
         self.accept()
 
     def receive(self, text_data):
-        text_data_json = json.loads(text_data)
-        message = text_data_json["message"]
-        username = self.user.username
+        received_json = json.loads(text_data)
+        message = received_json.get("message")
+        sending_user_id = received_json.get("sent_by")
+        receiving_user_id = received_json.get("send_to")
+        thread_id = received_json.get("thread_id")
 
-        # Send message to room group
-        async_to_sync(self.channel_layer.group_send)(
-            self.room_group_name,
-            {"type": "chat.message", "message": message, "username": username},
+        if not message:
+            print("Error: empty message")
+            return False
+
+        sending_user_instance = self.get_user_instance(sending_user_id)
+        receiving_user_instance = self.get_user_instance(receiving_user_id)
+
+        thread_instance = self.get_thread(thread_id)
+
+        if not sending_user_instance:
+            print("Error: SUI is incorrect")
+        if not receiving_user_instance:
+            print("Error: RUI is incorrect")
+        if not thread_instance:
+            print("Error: thread instance is incorrect")
+
+        self.create_chat_message(thread_instance, sending_user_instance, message)
+
+        target_chat_room = f'user_chatroom_{receiving_user_id}'
+
+        response_object = {
+            "type": "chat.message",
+            "message": message,
+            "sent_by": self.user.id,
+            "thread_id": thread_id,
+        }
+
+        # Send to self chat room
+
+        self.channel_layer.group_send(
+            self.chat_room,
+            response_object
         )
 
-    # Receive message from room group
+        # Send to target chat room
+        self.channel_layer.group_send(
+            target_chat_room,
+            response_object
+        )
+
     def chat_message(self, event):
-        message = event["message"]
-        username = event["username"]
-        # Send message to WebSocket
-        self.send(
-            text_data=json.dumps(
-                {
-                    "type": "chat",
-                    "message": message,
-                    "username": username,
-                }
-            )
-        )
+        self.send(text_data=json.dumps({
+            "type": "chat",
+            "message": event["message"],
+            "sent_by": event["sent_by"],
+            "thread_id": event["thread_id"]
+        }))
 
     def disconnect(self, close_code):
-        # Leave room group
-        async_to_sync(self.channel_layer.group_discard)(
-            self.room_group_name, self.channel_name
-        )
+        pass
+
+    def get_user_instance(self, user_id):
+        return db_retrieve_or_none(User, user_id)
+
+    def get_thread(self, thread_id):
+        return db_retrieve_or_none(Thread, thread_id)
+
+    def create_chat_message(self, thread, user, message):
+        ChatMessage.objects.create(thread=thread, user=user, message=message)
+
+
+
+
+
+
+
--- chat/models.py
+++ chat/models.py
@@ -1,3 +1,19 @@
 from django.db import models  # noqa
+from django.contrib.auth.models import User
 
 # Create your models here.
+
+class Thread(models.Model):
+    first_user = models.ForeignKey(User, on_delete=models.CASCADE, null=True, blank=True, related_name="chatroom_first_user")
+    second_user = models.ForeignKey(User, on_delete=models.CASCADE, null=True, blank=True, related_name="chatroom_second_user")
+    updated = models.DateTimeField(auto_now=True)
+    timestamp = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        unique_together = ["first_user", "second_user"]
+
+class ChatMessage(models.Model):
+    thread = models.ForeignKey(Thread, on_delete=models.CASCADE, null=True, blank=True, related_name="chat_message")
+    sending_user = models.ForeignKey(User, on_delete=models.CASCADE)
+    message = models.TextField()
+    timestamp = models.DateTimeField(auto_now_add=True)
