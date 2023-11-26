d1dc5b357c2fbab7330ee3893ce50db4673ea65e
Mike Zaharchenko <mz2433@nyu.edu>
2023-11-25 21:12:58 -0500

    black

--- chat/views.py
+++ chat/views.py
@@ -2,7 +2,12 @@ from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
 from .models import Thread
 
+
 @login_required
 def trip_messages_page(request):
-    threads = Thread.objects.by_user(user=request.user).prefetch_related('chat_message').order_by('timestamp')
-    return render(request, 'chat/room.html', {"threads": threads})
+    threads = (
+        Thread.objects.by_user(user=request.user)
+        .prefetch_related("chat_message")
+        .order_by("timestamp")
+    )
+    return render(request, "chat/room.html", {"threads": threads})
