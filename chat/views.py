7b678878604745d62fa0484f11b8a1aeb298d4cc
Mike Zaharchenko <mz2433@nyu.edu>
2023-11-27 01:07:09 -0500

    Quite a few changes- added a message room template for the chat itself and a threads template to select a chat. It's a little more intuitive for me to do it this way right now- we will change this to look like a real chat with the different threads on a side column later. Changed urls.py to include new routing and to take parameters that can be passed via a template argument. This lets me send data from the front end to the back end to then JSON-ify it and use it in the JS script easier. Altered view functions to accomodate jsonification

--- chat/views.py
+++ chat/views.py
@@ -1,13 +1,26 @@
+import json
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
 from .models import Thread
 
 
 @login_required
-def trip_messages_page(request):
+def threads_page(request):
     threads = (
         Thread.objects.by_user(user=request.user)
         .prefetch_related("chat_message")
         .order_by("timestamp")
     )
-    return render(request, "chat/room.html", {"threads": threads})
+    return render(request, "chat/threads.html", {"threads": threads})
+
+@login_required
+def messages_page(request, thread_id, other_user_id):
+    json_data = {
+        "thread_id": thread_id,
+        "other_user_id": other_user_id,
+        "self_user_id": request.user.id
+    }
+    context = {
+        "jsonData": json.dumps(json_data)
+    }
+    return render(request, "chat/")
