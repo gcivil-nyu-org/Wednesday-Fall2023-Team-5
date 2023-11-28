1b8a08cffae03ad9d30577b3721af9ce0af44bde
kravisankaran <kumudriyer@gmail.com>
2023-11-28 13:56:31 -0500

    fixes

--- Procfile
+++ Procfile
@@ -1,2 +1,1 @@
 web: gunicorn --bind :8000 --workers 1 --threads 2 soloconnect.wsgi:application
-websocket: daphne -b :: -p 5000 soloconnect.asgi:application
--- soloconnect/asgi.py
+++ soloconnect/asgi.py
@@ -9,15 +9,14 @@ https://docs.djangoproject.com/en/4.2/howto/deployment/asgi/
 
 import os
 
-from django.core.asgi import get_asgi_application
-
-
-from channels.routing import ProtocolTypeRouter, URLRouter
 from channels.auth import AuthMiddlewareStack
+from channels.routing import ProtocolTypeRouter, URLRouter
+from django.core.asgi import get_asgi_application
 from chat import routing
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "soloconnect.settings")
 
+django_asgi_app = get_asgi_application()
 
 application = ProtocolTypeRouter(
     {
