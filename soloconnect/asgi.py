960378dfd0d5d497b1731a09d2dcfb5eb9665829
kravisankaran <kumudriyer@gmail.com>
2023-11-28 12:02:59 -0500

    fixing flake

--- soloconnect/asgi.py
+++ soloconnect/asgi.py
@@ -12,8 +12,6 @@ import os
 from django.core.asgi import get_asgi_application
 
 
-django_asgi_app = get_asgi_application()
-
 from channels.routing import ProtocolTypeRouter, URLRouter
 from channels.auth import AuthMiddlewareStack
 from chat import routing
@@ -23,7 +21,7 @@ os.environ.setdefault("DJANGO_SETTINGS_MODULE", "soloconnect.settings")
 
 application = ProtocolTypeRouter(
     {
-        "http": django_asgi_app,
+        "http": get_asgi_application(),
         "websocket": AuthMiddlewareStack(URLRouter(routing.websocket_urlpatterns)),
     }
 )
