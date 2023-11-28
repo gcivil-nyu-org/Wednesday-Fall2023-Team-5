a1d5c222d96ec8ca2db86fc3844df6facbf492ad
kravisankaran <kumudriyer@gmail.com>
2023-11-28 11:49:57 -0500

    adding a procfile

--- soloconnect/settings.py
+++ soloconnect/settings.py
@@ -175,6 +175,7 @@ EMAIL_HOST_USER = os.getenv("EMAIL_HOST_USER")
 EMAIL_HOST_PASSWORD = os.getenv("EMAIL_HOST_PASSWORD")
 EMAIL_PORT = 587
 
+
 CHANNEL_LAYERS = {
     "default": {
         "BACKEND": "channels_redis.core.RedisChannelLayer",
