47e0b868885d3e64fb0a66048af72de7f84ff488
sawyer1997 <rishabhverma220898@gmail.com>
2023-11-29 17:22:39 -0500

    Added url and linked view function to unmatch matched user

--- matching/urls.py
+++ matching/urls.py
@@ -22,4 +22,6 @@ urlpatterns = [
     path("react_match_req/", react_pending_request, name="react_request"),
 
     path("show_matches/", show_matches, name="show_matches"),
+
+    path("unmatch/", unmatch, name="unmatch"),
 ]
