2eede07f0603cfcff8ca944c9ae8f3f2dac61e7a
sawyer1997 <rishabhverma220898@gmail.com>
2023-11-29 17:26:43 -0500

    fixed black on matching/urls

--- matching/urls.py
+++ matching/urls.py
@@ -6,22 +6,18 @@ from .views import (
     show_pending_requests,
     react_pending_request,
     show_matches,
-    unmatch
+    unmatch,
 )
 
 app_name = "matching"
 urlpatterns = [
-    path("show_potential_matches/", show_potential_matches, name="show_potential_matches"),
-
+    path(
+        "show_potential_matches/", show_potential_matches, name="show_potential_matches"
+    ),
     path("show_pending_requests/", show_pending_requests, name="show_pending_requests"),
-
     path("send_match_req/", send_matching_request, name="send_request"),
-
     path("cancel_match_req/", cancel_matching_request, name="cancel_request"),
-
     path("react_match_req/", react_pending_request, name="react_request"),
-
     path("show_matches/", show_matches, name="show_matches"),
-
     path("unmatch/", unmatch, name="unmatch"),
 ]
