eba257fc2c7deb8ab4ad7766c530b1942f3e7984
sawyer1997 <rishabhverma220898@gmail.com>
2023-12-06 09:26:48 -0500

    Increased coverage for send matching request

--- matching/tests.py
+++ matching/tests.py
@@ -8,37 +8,35 @@ from django.contrib.messages import get_messages
 
 
 class TestMatchingViews(TestCase):
-
     def setUp(self):
         self.client = Client()
-        self.password = 'matching_password'
+        self.password = "matching_password"
         self.user1 = User.objects.create_user(
-            username='matching_user',
-            email='matching_user@nyu.edu',
+            username="matching_user",
+            email="matching_user@nyu.edu",
             password=self.password,
         )
         self.user1.userprofile.dob = datetime(1997, 8, 22)
         self.user1.save()
         self.user2 = User.objects.create_user(
-            username='matching_user2',
-            email='matching_user2@nyu.edu',
-            password=self.password
+            username="matching_user2",
+            email="matching_user2@nyu.edu",
+            password=self.password,
         )
         self.user2.userprofile.dob = datetime(1997, 8, 22)
         self.user2.save()
         self.user3 = User.objects.create_user(
-            username='matching_user3',
-            email='matching_user3@nyu.edu',
-            password=self.password
+            username="matching_user3",
+            email="matching_user3@nyu.edu",
+            password=self.password,
         )
         self.user3.userprofile.dob = datetime(1997, 8, 22)
         self.user3.save()
-        city = [('Bangalore', 'Bangalore')]
-        country = [('India', 'India')]
+        city = [("Bangalore", "Bangalore")]
+        country = [("India", "India")]
         companion = ("Companion", "Companion")
         trip_destination = Trip.objects.create(
-            destination_city=city,
-            destination_country=country
+            destination_city=city, destination_country=country
         )
         self.utrip1 = UserTrip.objects.create(
             start_trip=datetime(2023, 12, 18),
@@ -64,14 +62,16 @@ class TestMatchingViews(TestCase):
 
     def test_show_potential_matches(self):
         user1login = self.client.login(
-            username='matching_user',
+            username="matching_user",
             password=self.password,
         )
         self.assertTrue(user1login)
         response = self.client.get(
-            reverse("matching:show_potential_matches", kwargs={"utrip_id": self.utrip1.id})
+            reverse(
+                "matching:show_potential_matches", kwargs={"utrip_id": self.utrip1.id}
+            )
         )
-        matching_users = [mu['user'] for mu in response.context['matching_users']]
+        matching_users = [mu["user"] for mu in response.context["matching_users"]]
 
         self.assertEqual(response.status_code, 200)
         self.assertTemplateUsed(template_name="matching/list_potential_matches.html")
@@ -79,66 +79,107 @@ class TestMatchingViews(TestCase):
         self.assertIn(self.user2, matching_users)
         self.assertIn(self.user3, matching_users)
         self.client.logout()
-        user = self.client.session.get('_auth_user_id')
+        user = self.client.session.get("_auth_user_id")
         self.assertIsNone(user)
 
         user2login = self.client.login(
-            username='matching_user2',
+            username="matching_user2",
             password=self.password,
         )
         self.assertTrue(user2login)
         response = self.client.get(
-            reverse("matching:show_potential_matches", kwargs={"utrip_id": self.utrip2.id})
+            reverse(
+                "matching:show_potential_matches", kwargs={"utrip_id": self.utrip2.id}
+            )
         )
-        matching_users = [mu['user'] for mu in response.context['matching_users']]
+        matching_users = [mu["user"] for mu in response.context["matching_users"]]
         self.assertEqual(response.status_code, 200)
         self.assertIn(self.user1, matching_users)
         self.assertNotIn(self.user2, matching_users)
         self.assertIn(self.user3, matching_users)
+
+        response = self.client.get(
+            reverse(
+                "matching:show_potential_matches", kwargs={"utrip_id": self.utrip1.id}
+            ),
+            follow=True
+        )
+        self.assertEqual(response.status_code, 403)
+        self.assertTemplateUsed(template_name="trip/view_trips.html")
         self.client.logout()
-        user = self.client.session.get('_auth_user_id')
+        user = self.client.session.get("_auth_user_id")
         self.assertIsNone(user)
 
     def test_send_matching_request(self):
         user1login = self.client.login(
-            username='matching_user',
+            username="matching_user",
             password=self.password,
         )
         self.assertTrue(user1login)
         response = self.client.post(
             reverse("matching:send_request", kwargs={"utrip_id": self.utrip1.id}),
-            {
-                'receiver_uid': self.user2.id,
-                'receiver_utrip_id': self.utrip2.id
-            },
-            follow=True
+            {"receiver_uid": self.user2.id, "receiver_utrip_id": self.utrip2.id},
+            follow=True,
         )
         messages = list(get_messages(response.wsgi_request))
         self.assertIn(response.status_code, [200, 302])
         self.assertTrue(messages)
-        self.assertEqual(messages[0].message, 'Matching request sent to user')
-        resp_up = response.context['matching_users']
+        self.assertEqual(messages[0].message, "Matching request sent to user")
+        resp_up = response.context["matching_users"]
         for mu in resp_up:
-            if mu['user'] == self.user2:
-                self.assertTrue(mu['sent_match'], False)
+            if mu["user"] == self.user2:
+                self.assertTrue(mu["sent_match"], False)
                 break
+        self.utrip2.is_active = False
+        self.utrip2.save()
+        response = self.client.post(
+            reverse("matching:send_request", kwargs={"utrip_id": self.utrip1.id}),
+            {"receiver_uid": self.user2.id, "receiver_utrip_id": self.utrip2.id},
+            follow=True,
+        )
+        messages = list(get_messages(response.wsgi_request))
+        self.assertIn(response.status_code, [200, 302])
+        self.assertEqual(
+            messages[0].message, "The receiver or their trip is not active anymore."
+        )
         self.client.logout()
 
+        self.utrip2.is_active = True
+        self.utrip2.save()
         user2login = self.client.login(
-            username='matching_user2',
+            username="matching_user2",
             password=self.password,
         )
         self.assertTrue(user2login)
         response = self.client.post(
             reverse("matching:send_request", kwargs={"utrip_id": self.utrip2.id}),
             {
-                'receiver_uid': self.user1.id,
-                'receiver_utrip_id': self.utrip1.id,
+                "receiver_uid": self.user1.id,
+                "receiver_utrip_id": self.utrip1.id,
             },
-            follow=True
+            follow=True,
         )
         self.assertIn(response.status_code, [200, 302])
         messages = list(get_messages(response.wsgi_request))
         self.assertTrue(len(messages), 1)
-        self.assertEqual(messages[0].message, "The receiver might already have sent a matching "
-                                              "request to you, please check your pending matches")
+        self.assertEqual(
+            messages[0].message,
+            "The receiver might already have sent a matching "
+            "request to you, please check your pending matches",
+        )
+        response = self.client.post(
+            reverse("matching:send_request", kwargs={"utrip_id": self.utrip1.id}),
+            {
+                "receiver_uid": self.user1.id,
+                "receiver_utrip_id": self.utrip1.id,
+            },
+            follow=True,
+        )
+        self.assertEqual(response.status_code, 403)
+        messages = list(get_messages(response.wsgi_request))
+        self.assertTrue(len(messages), 1)
+        self.assertEqual(
+            messages[0].message,
+            "You are not allowed to edit this.",
+        )
+        self.assertTemplateUsed(response=None, template_name="trip/view_trips.html")
