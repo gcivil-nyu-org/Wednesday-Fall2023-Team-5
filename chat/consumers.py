ec97abbd7a98bed24e66d97b15736f5f67d0a7b3
kravisankaran <kumudriyer@gmail.com>
2023-12-10 23:25:48 -0500

    combined alignment and profile pic

--- chat/consumers.py
+++ chat/consumers.py
@@ -90,7 +90,13 @@ class ChatConsumer(WebsocketConsumer):
         sending_user_instance = self.get_user_instance(sending_user_id)
         receiving_user_instance = self.get_user_instance(receiving_user_id)
 
+        sender_image_url = ""
         thread_instance = self.get_thread(thread_id)
+
+        if sending_user_instance == thread_instance.first_user:
+            sender_image_url = thread_instance.first_user_image_url
+        elif sending_user_instance == thread_instance.second_user:
+            sender_image_url = thread_instance.second_user_image_url
 
         if not sending_user_instance:
             print("Error: SUI is incorrect")
@@ -114,6 +120,7 @@ class ChatConsumer(WebsocketConsumer):
                 "user_name": sending_user_instance.username,
                 "first_name": sending_user_instance.first_name,
                 "last_initial": sending_user_instance.last_name[0],
+                "sender_image_url": sender_image_url
             },
         )
         print("user name: " + sending_user_instance.username)
@@ -128,6 +135,7 @@ class ChatConsumer(WebsocketConsumer):
                 "user_name": sending_user_instance.username,
                 "first_name": sending_user_instance.first_name,
                 "last_initial": sending_user_instance.last_name[0],
+                "sender_image_url": sender_image_url
             },
         )
 
@@ -145,6 +153,7 @@ class ChatConsumer(WebsocketConsumer):
                     "user_name": event["user_name"],
                     "first_name": event["first_name"],
                     "last_initial": event["last_initial"],
+                    "sender_image_url": event["sender_image_url"]
                 }
             )
         )
@@ -159,22 +168,20 @@ class ChatConsumer(WebsocketConsumer):
         return db_retrieve_or_none(Thread, thread_id)
 
     def create_chatmessage_object(self, thread, sending_user, message, receiving_user):
-        sending_image_url = ""
-        receiving_image_url = ""
-        sender_image = UserImages.objects.filter(Q(user_profile_id=sending_user.id))
-        receiver_image = UserImages.objects.filter(Q(user_profile=receiving_user.id))
-        sender_instances = [UserImages(**item) for item in sender_image.values()]
-        receiver_instances = [UserImages(**item) for item in receiver_image.values()]
-        if len(sender_instances) > 0:
-            sending_image_url = sender_instances[0].get_absolute_url()
-            print(sending_image_url)
-        if len(receiver_instances) > 0:
-            receiving_image_url = receiver_instances[0].get_absolute_url()
-            print(receiving_image_url)
+        # sending_image_url = ""
+        # receiving_image_url = ""
+        # sender_image = UserImages.objects.filter(Q(user_profile_id=sending_user.id))
+        # receiver_image = UserImages.objects.filter(Q(user_profile=receiving_user.id))
+        # sender_instances = [UserImages(**item) for item in sender_image.values()]
+        # receiver_instances = [UserImages(**item) for item in receiver_image.values()]
+        # if len(sender_instances) > 0:
+        #     sending_image_url = sender_instances[0].get_absolute_url()
+        #     print(sending_image_url)
+        # if len(receiver_instances) > 0:
+        #     receiving_image_url = receiver_instances[0].get_absolute_url()
+        #     print(receiving_image_url)
         ChatMessage.objects.create(
             thread=thread,
             sending_user=sending_user,
-            message=message,
-            sending_image_url=sending_image_url,
-            receiving_image_url=receiving_image_url
+            message=message
         )
